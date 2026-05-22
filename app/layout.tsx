e7c006b947999900a1a74883f3b07f8dc4274fe5
Sunita Sinha <sunitasinha@Sunitas-MacBook-Pro.local>
2026-05-22 16:33:36 +0800

    Remove unused fs/promises import from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter, Playfair_Display } from 'next/font/google'
 import './globals.css'
-import { link } from 'fs/promises'
 
 const inter = Inter({
   subsets: ['latin'],
